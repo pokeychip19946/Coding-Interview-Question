3d6063683988a54af3eeed4d972a812761815da6
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-08-24 12:20:50 +0600

    Update leetcode-169-majority-element.java

--- LeetCode Solution/leetcode-169-majority-element.java
+++ LeetCode Solution/leetcode-169-majority-element.java
@@ -1,10 +1,29 @@
-/*
-  LeetCode Solution PlayList: 
-*/
+
 /*
   Video Explanation: https://youtu.be/WmxDyiXlsAE
 */
 
+// O(n) Time
+// O(1) Space
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
+
+// HashMap solution
+// O(n) Time
+// O(n) Space
 class Solution {
     public int majorityElement(int[] nums) {
         Map<Integer, Integer> hash_map = new HashMap<Integer, Integer>();
