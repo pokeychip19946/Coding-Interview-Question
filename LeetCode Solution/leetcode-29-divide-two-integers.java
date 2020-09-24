bedc478be31747baa452f9b34acafc8c59525ece
Md. A. Barik <68262744+techbarik@users.noreply.github.com>
2020-09-24 17:56:32 +0600

    Update leetcode-29-divide-two-integers.java

--- LeetCode Solution/leetcode-29-divide-two-integers.java
+++ LeetCode Solution/leetcode-29-divide-two-integers.java
@@ -22,3 +22,18 @@ class Solution {
         return  isNegative ? (int) ~result + 1 : result > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) result;
     }
 }
+
+/***Intuitive Solution Without long***/
+
+class Solution {
+      public int divide(int A, int B) {
+        if (A == 1 << 31 && B == -1) return (1 << 31) - 1;
+        int a = Math.abs(A); b = Math.abs(B), res = 0, x = 0;
+        while (a - b >= 0) {
+            for (x = 0; a - (b << x << 1) >= 0; x++);
+            res += 1 << x;
+            a -= b << x;
+        }
+        return (A > 0) == (B > 0) ? res : -res;
+    }
+}
